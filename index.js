8ac7ddb238cdb65b0bac0095b8f800b25159358f
mikhail-cct <tayemnyj+cct@gmail.com>
2021-11-22 16:42:34 +0000

    index.js

--- index.js
+++ index.js
@@ -4,6 +4,29 @@ const   http = require('http'), //This module provides the HTTP server functiona
         fs = require('fs'), //This module allows to work with the file system: read and write files back
         xmlParse = require('xslt-processor').xmlParse, //This module allows to work with XML files
         xsltProcess = require('xslt-processor').xsltProcess, //The same module allows us to uitlise XSL Transformations
-        xml2js = require('xml2js') //This module does XML <-> JSON conversion
+        xml2js = require('xml2js'); //This module does XML <-> JSON conversion
 
+const   router = express(), 
+        server = http.createServer(router);
+
+router.get('/', function(req, res) {
+
+    res.writeHead(200, {'Content-Type' : 'text/html'});
+
+    let xml = fs.readFileSync('PaddysCafe.xml', 'utf8'),
+        xsl = fs.readFileSync('PaddysCafe.xsl', 'utf8');
+
+    let doc = xmlParse(xml),
+        stylesheet = xmlParse(xsl);
+
+    let result = xsltProcess(doc, stylesheet);
+
+    res.end(result.toString());
+
+});
+
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
+    const addr = server.address();
+    console.log("Server listening at", addr.address + ":" + addr.port)
+});
 
