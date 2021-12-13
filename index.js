92d7b38eedbb5829844cd5d06ef03840ddd0c934
mikhail-cct <tayemnyj+cct@gmail.com>
2021-12-13 17:02:58 +0000

    wk13 final code

--- index.js
+++ index.js
@@ -10,6 +10,26 @@ const   router = express(),
         server = http.createServer(router);
 
 router.use(express.static(path.resolve(__dirname,'views'))); //We serve static content from "views" folder
+router.use(express.urlencoded({extended: true})); //We allow the data sent from the client to be encoded in a URL targeting our end point
+router.use(express.json()); //We include support for JSON
+
+// Function to read in XML file and convert it to JSON
+function XMLtoJSON(filename, cb) {
+    var filepath = path.normalize(path.join(__dirname, filename));
+    fs.readFile(filepath, 'utf8', function(err, xmlStr) {
+      if (err) throw (err);
+      xml2js.parseString(xmlStr, {}, cb);
+    });
+};
+  
+  //Function to convert JSON to XML and save it
+function JSONtoXML(filename, obj, cb) {
+    var filepath = path.normalize(path.join(__dirname, filename));
+    var builder = new xml2js.Builder();
+    var xml = builder.buildObject(obj);
+    fs.unlinkSync(filepath);
+    fs.writeFile(filepath, xml, cb);
+};
 
 router.get('/get/html', function(req, res) {
 
@@ -32,6 +52,56 @@ router.get('/get/html', function(req, res) {
     console.log(result);
 
     res.end(result.toString());
+
+});
+
+router.post('/post/json', function (req, res) {
+
+    function appendJSON(obj) {
+
+        console.log(obj)
+
+        XMLtoJSON('PaddysCafe.xml', function (err, result) {
+            if (err) throw (err);
+            
+            result.menu.section[obj.sec_n].entry.push({'item': obj.item, 'price': obj.price});
+
+            console.log(JSON.stringify(result, null, "  "));
+
+            JSONtoXML('PaddysCafe.xml', result, function(err){
+                if (err) console.log(err);
+            });
+        });
+    };
+
+    appendJSON(req.body);
+
+    res.redirect('back');
+
+});
+
+router.post('/post/delete', function (req, res) {
+
+    function deleteJSON(obj) {
+
+        console.log(obj)
+
+        XMLtoJSON('PaddysCafe.xml', function (err, result) {
+            if (err) throw (err);
+            
+            delete result.menu.section[obj.section].entry[obj.entree];
+
+            console.log(JSON.stringify(result, null, "  "));
+
+            JSONtoXML('PaddysCafe.xml', result, function(err){
+                if (err) console.log(err);
+            });
+        });
+    };
+
+    deleteJSON(req.body);
+
+    res.redirect('back');
 
 });
 
